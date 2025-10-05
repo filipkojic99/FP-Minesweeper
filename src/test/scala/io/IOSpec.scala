d76fc461a29e90c4c3552f00f2000777073bffd6
Filip Kojić <filipkojic48@gmail.com>
2025-10-05 16:58:45 +0200

    Complete testing for the game

--- src/test/scala/io/IOSpec.scala
+++ src/test/scala/io/IOSpec.scala
@@ -1,5 +1,6 @@
 package io
 
+import model.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -29,5 +30,45 @@ class IOSpec extends AnyFlatSpec with Matchers {
       ('L', 1, 1),
       ('L', 2, 4)
     )
+  }
+
+  "GameIO.save/load" should "save and load a simple game correctly" in {
+    val board = Board(Vector(
+      Vector(Cell(CellContent.Clear, 1), Cell(CellContent.Mine, 0)),
+      Vector(Cell(CellContent.Clear, 1), Cell(CellContent.Clear, 1))
+    ))
+
+    val state = Vector(
+      Vector(CellState.Hidden, CellState.Flagged),
+      Vector(CellState.Revealed, CellState.Hidden)
+    )
+
+    val gs = GameState(
+      board = board,
+      state = state,
+      status = GameStatus.InProgress,
+      clicks = 4,
+      startedAtMs = System.currentTimeMillis() - 5000,
+      endedAtMs = None,
+      hintsUsed = 2,
+      score = None,
+      elapsedSavedSec = 12
+    )
+
+    val path = "saves/test.txt"
+
+    GameIO.save(path, gs, "src/test/resources/testIO.txt")
+
+    val (loaded, levelPath) = GameIO.load(path)
+
+    levelPath shouldBe "src/test/resources/testIO.txt"
+    loaded.status shouldBe gs.status
+    loaded.clicks shouldBe gs.clicks
+    loaded.hintsUsed shouldBe gs.hintsUsed
+    loaded.state shouldBe gs.state
+    loaded.board.rows shouldBe 2
+    loaded.board.cols shouldBe 2
+    loaded.score shouldBe None
+    loaded.endedAtMs shouldBe None
   }
 }
